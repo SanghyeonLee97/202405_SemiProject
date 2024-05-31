3f50ffcabbd30645c25bfbd88bee70fa694c28cf
Cheonrihyang <tldn0631@gmail.com>
2024-05-31 10:38:14 +0900

    dto생성

---
+++ src/DTO/CommunitySearchDTO.java
@@ -0,0 +1,25 @@
+package DTO;
+
+public class CommunitySearchDTO {
+	String CommunitySearchDTOBoard;
+	String CommunitySearchDTOSelect;
+	String CommunitySearchDTOSearch;
+	public String getCommunitySearchDTOBoard() {
+		return CommunitySearchDTOBoard;
+	}
+	public void setCommunitySearchDTOBoard(String communitySearchDTOBoard) {
+		CommunitySearchDTOBoard = communitySearchDTOBoard;
+	}
+	public String getCommunitySearchDTOSelect() {
+		return CommunitySearchDTOSelect;
+	}
+	public void setCommunitySearchDTOSelect(String communitySearchDTOSelect) {
+		CommunitySearchDTOSelect = communitySearchDTOSelect;
+	}
+	public String getCommunitySearchDTOSearch() {
+		return CommunitySearchDTOSearch;
+	}
+	public void setCommunitySearchDTOSearch(String communitySearchDTOSearch) {
+		CommunitySearchDTOSearch = communitySearchDTOSearch;
+	}
+}
