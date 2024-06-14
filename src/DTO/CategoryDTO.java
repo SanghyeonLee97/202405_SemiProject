6df58b27d65851ae745a3eb304cfcbfd07eefb63
Sanghyeon Lee <ledlee.97@gmail.com>
2024-06-14 10:42:17 +0900

    Product 관련 DTO 생성

---
+++ src/DTO/CategoryDTO.java
@@ -0,0 +1,21 @@
+package DTO;
+
+public class CategoryDTO {
+	private int pc_no;
+	private String pc_name;
+	
+	
+	public int getPc_no() {
+		return pc_no;
+	}
+	public void setPc_no(int pc_no) {
+		this.pc_no = pc_no;
+	}
+	public String getPc_name() {
+		return pc_name;
+	}
+	public void setPc_name(String pc_name) {
+		this.pc_name = pc_name;
+	}
+
+}
