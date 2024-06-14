5e1023dfb16ac340781c774cca8a8ec102b3500b
Sanghyeon Lee <ledlee.97@gmail.com>
2024-06-14 10:43:03 +0900

    안쓰는기능 삭제

--- src/controller/ProductCommand.java
+++ src/controller/ProductCommand.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.CommandProsessor;
-import model.NoticeRead;
 import model.product.ProductList;
 
 @WebServlet("*.pdo")
