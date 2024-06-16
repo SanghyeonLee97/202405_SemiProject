7512d08f0038467af20e842bef1c1822dec33e50
Sanghyeon Lee <ledlee.97@gmail.com>
2024-06-16 22:43:11 +0900

    상품상세페이지 컨트롤러 수정

--- src/controller/controller_module/Command_Product.java
+++ src/controller/controller_module/Command_Product.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.CommandProsessor;
+import model.product.ProductDetail;
 import model.product.ProductList;
 
 public class Command_Product extends Command_Parents{
@@ -12,6 +13,8 @@ public class Command_Product extends Command_Parents{
 	public CommandProsessor command_Operate(HttpServletRequest req, HttpServletResponse resp, String servletPath) {
 		if(servletPath.contains("/product/list.do")) {
 			processor = new ProductList();
+		}else if(servletPath.contains("/details.do")) {
+			processor = new ProductDetail();
 		}else {
 			System.out.println("매칭되는 서블릿 없음");
 		}
