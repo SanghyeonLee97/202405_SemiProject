f0c2f8deaf741722740cbb7826df15999668767c
tldn0631 <tldn0631@gmail.com>
2024-06-20 12:34:25 +0900

    쿠폰페이지 구현

--- src/model/mypage/MypageCoupon.java
+++ src/model/mypage/MypageCoupon.java
@@ -1,15 +1,30 @@
 package model.mypage;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import DTO.MyPageCouponDTO;
+import dao.CommunityDAO;
+import dao.MyPageDAO;
 import model.CommandProsessor;
 
 public class MypageCoupon implements CommandProsessor{
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse resp) {
-
+		CommunityDAO cdao = new CommunityDAO();
+		MyPageDAO mdao = new MyPageDAO();
+		HttpSession session = req.getSession();
+		String uId = (String)session.getAttribute("id");
+		int uNo = cdao.getCustomerNo(uId);
+		
+		ArrayList<MyPageCouponDTO> mcdtoArr = mdao.getMypageCoupon(uNo);
+		
+		req.setAttribute("mcdtoArr", mcdtoArr);
+		
 		return "/mypage/mypage_coupon.jsp";
 	}
 
