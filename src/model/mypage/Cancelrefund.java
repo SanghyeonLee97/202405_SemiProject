fddfbee0baba45494f9ddad41b0fc82955ba3033
tldn0631 <tldn0631@gmail.com>
2024-06-20 17:28:17 +0900

    코드정리

--- src/model/mypage/Cancelrefund.java
+++ src/model/mypage/Cancelrefund.java
@@ -4,12 +4,8 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import DTO.MyPageCancelDTO;
-import dao.CommunityDAO;
-import dao.MyPageDAO;
-import model.CommandProsessor;
 
 public class Cancelrefund extends MyPage{
 
