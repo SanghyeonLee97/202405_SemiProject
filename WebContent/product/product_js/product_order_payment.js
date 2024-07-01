ca6b63f857d3ff9ffab1df28d37a99293a0187d1
Sanghyeon Lee <ledlee.97@gmail.com>
2024-07-01 17:10:27 +0900

    쿠폰, 적립금 사용시 음수가 되지 않도록 처리

--- WebContent/product/product_js/product_order_payment.js
+++ WebContent/product/product_js/product_order_payment.js
@@ -15,6 +15,7 @@ function updateDiscount() {
 	let pointValue = parseInt($('input[name="pointUsage"]').val()) || 0;
 	let maxPoints = parseInt($('#maxPoints').val());
 	
+	//보유한 적립금보다 큰 금액을 입력할 경우
 	if (pointValue > maxPoints) {
 		alert('보유한 적립금보다 많이 사용할 수 없습니다.');
 		$('input[name="pointUsage"]').val(maxPoints);
@@ -29,6 +30,15 @@ function updateDiscount() {
 	let totalDiscount = automaticDiscount + couponDiscount + pointValue;
 	let totalPrice = (productPrice * productQuantity) - totalDiscount;
 	
+	// 총 결제금액이 음수가 되지 않도록 처리
+    if (totalPrice < 0) {
+        alert('할인 및 적립금을 사용한 총 결제금액은 0원 이상이어야 합니다.');
+        pointValue = pointValue - Math.abs(totalPrice);
+        totalDiscount = automaticDiscount + couponDiscount + pointValue;
+        totalPrice = 0;
+        $('input[name="pointUsage"]').val(pointValue);
+    }
+    
 	$('#totalDiscount').text(totalDiscount + '원');
     $('#totalPrice').text(totalPrice + '원');
 }
